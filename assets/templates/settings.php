e04a65791dd6c16875c44e55b238b7df3ca88711
Christian Wach <needle@haystack.co.uk>
2015-01-29 13:07:59 +0000

    update settings page text

--- assets/templates/settings.php
+++ assets/templates/settings.php
@@ -19,6 +19,10 @@
 
 		<?php wp_nonce_field( 'bp_groups_civicrm_sync_settings_action', 'bp_groups_civicrm_sync_nonce' ); ?>
 
+		<p><?php _e( '<strong>Please Note:</strong> it is strongly recommended to choose the following settings before you sync groups. You can change these settings later, but it will require some heavy processing if you have a large number of groups.', 'bp-groups-civicrm-sync' ); ?></p>
+
+		<hr>
+
 		<h3><?php _e( 'Parent Group', 'bp-groups-civicrm-sync' ); ?></h3>
 
 		<p><?php _e( 'Depending on your use case, select whether you want your CiviCRM groups to be assigned to a "BuddyPress Groups" parent group in CiviCRM. If you do, then CiviCRM groups will be nested under - and inherit permissions from - the "BuddyPress Groups" parent group. Please refer to <a href="http://book.civicrm.org/user/current/organising-your-data/groups-and-tags/">the documentation</a> to decide if this is useful to you or not.', 'bp-groups-civicrm-sync' ); ?></p>
@@ -36,6 +40,8 @@
 		</table>
 
 		<?php if ( $bp_group_hierarchy ) : ?>
+			<hr>
+
 			<h3><?php _e( 'BuddyPress Group Hierarchy', 'bp-groups-civicrm-sync' ); ?></h3>
 
 			<p><?php _e( 'Depending on your use case, select whether you want your CiviCRM groups to be hierarchically organised in CiviCRM. If you do, then CiviCRM groups will be nested under one another, mirroring the BuddyPress Group Hierarchy. Again, please refer to <a href="http://book.civicrm.org/user/current/organising-your-data/groups-and-tags/">the documentation</a> to decide if this is useful to you or not.', 'bp-groups-civicrm-sync' ); ?></p>
@@ -52,6 +58,8 @@
 
 			</table>
 		<?php endif; ?>
+
+		<hr>
 
 		<p class="submit">
 			<input class="button-primary" type="submit" id="bp_groups_civicrm_sync_settings_submit" name="bp_groups_civicrm_sync_settings_submit" value="<?php _e( 'Save Changes', 'bp-groups-civicrm-sync' ); ?>" />
