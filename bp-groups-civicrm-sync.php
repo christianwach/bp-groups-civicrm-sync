9f425e534ad11ed2cd37e097aa046c98bf179ca1
Christian Wach <needle@haystack.co.uk>
2024-12-17 11:34:06 +0000

    Update plugin header

--- bp-groups-civicrm-sync.php
+++ bp-groups-civicrm-sync.php
@@ -16,7 +16,19 @@
  * Text Domain:       bp-groups-civicrm-sync
  * Domain Path:       /languages
  *
- * @package BP_Groups_CiviCRM_Sync
+ * @package Transition_Resources
+ * @link    https://github.com/christianwach/bp-groups-civicrm-sync
+ * @license GPL v2 or later
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 // Exit if accessed directly.
